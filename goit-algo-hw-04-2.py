5f8e17c9654384f3418ecb6ef3168979759bbe05
SolarHaema <97592248+SolarHaema@users.noreply.github.com>
2024-07-08 20:40:02 +0300

    Update goit-algo-hw-04-2.py

--- goit-algo-hw-04-2.py
+++ goit-algo-hw-04-2.py
@@ -1,1 +1,33 @@
 
+
+def get_cats_info(path):
+    cats_info = []
+    
+    try:
+        file_path = Path(path)
+        
+        if not file_path.exists():
+            print(f"File not found: {file_path}")
+            return cats_info
+        
+        with file_path.open('r', encoding='utf-8') as file:
+            for line in file:
+                line = line.strip()
+                if line:
+                    parts = line.split(',')
+                    if len(parts) == 3:
+                        cat_id, name, age = parts
+                        try:
+                            age = int(age)
+                            cat_info = {"id": cat_id, "name": name, "age": age}
+                            cats_info.append(cat_info)
+    
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
+    return cats_info
+
+file_path = 'E:/mygame/cats.txt'
+cats = get_cats_info(file_path)
+for cat in cats:
+    print(cat)
